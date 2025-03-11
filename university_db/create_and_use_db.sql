2695b932883fd2c48cdb9a9abea9caf283002a78
allysasaulong <saulongma.allysa_bsit@plmun.edu.ph>
2025-03-11 14:51:28 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 01

--- university_db/create_and_use_db.sql
+++ university_db/create_and_use_db.sql
@@ -1,6 +1,5 @@
 -- Step 1: Create a database called UniversityDB
-CREATE DATABASE `UniversityDB`;
--- Step 2: Use the newly created database
+CREATE DATABASE `UniversityDB`;-- Step 2: Use the newly created database
 USE `UniversityDB`;
 
 -- Verification (Optional): 
